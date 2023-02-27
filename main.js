8c6c4ac9382a0306bad27e63adc267c2f76f7008
Zezheng Jiang <Zjian107@syr.edu>
2023-02-26 22:07:08 -0500

    conflict00

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 console.log("Daniel solves the issue with this pull reuqest!");
 console.log("Actually!");
+console.log(
+  "Daniel makes a change that the branch will not know, we will see conflicts here!"
+);
